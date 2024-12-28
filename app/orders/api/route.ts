b00e6491cdb2994f3a59ea4a800cb25d7ae4c835
ferMadrid92 <fer.madridb@gmail.com>
2024-12-28 01:17:32 -0500

    fix

--- app/orders/api/route.ts
+++ app/orders/api/route.ts
@@ -1,5 +1,7 @@
 import { prisma } from "@/src/lib/prisma";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     const orders = await prisma.order.findMany({
         take: 5, // Limit to 5 orders - adjust as needed
